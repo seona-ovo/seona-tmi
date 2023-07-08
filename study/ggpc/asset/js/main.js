3853814ec66caa754d26c5197d538d88ac0b003d
Cha Seona <peche0345@naver.com>
2023-07-08 13:19:22 +0900

    오랜만

---
+++ study/ggpc/asset/js/main.js
@@ -0,0 +1,19 @@
+const mainSlide = new Swiper('.sc-visual .swiper',{
+    navigation:{
+        prevEl:'.prev',
+        nextEl:'.next'
+    },
+    pagination:{
+        el:'.pagination'
+    }
+});
+
+const reviewSlide = new Swiper('.sc-review .swiper',{
+    navigation:{
+        prevEl:'.prev',
+        nextEl:'.next'
+    },
+    pagination:{
+        el:'.pagination'
+    }
+})
